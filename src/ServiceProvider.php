ffd4ce9dbe02a502728cd45df3cf6dfebd8181af
Rolin Bos <rolinbos1@icloud.com>
2022-07-19 10:48:10 +0200

    Init

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RayNl\SendcloudForSimpleCommerce;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
